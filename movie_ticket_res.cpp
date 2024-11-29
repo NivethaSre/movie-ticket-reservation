5b1d594ae81b57c195699168ce439748900657ed
NivethaSre <71762233028@cit.edu.in>
2024-11-29 16:00:11 +0530

    cinema_seat_management.c

--- movie_ticket_res.cpp
+++ movie_ticket_res.cpp
@@ -1,168 +1,170 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
-#include<string.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<string> vs;
-#define FOR(i,a,b) for(ll i = a;i<b;++i)
-#define pb push_back
-vector< pair<string, vector<vll > > > v;
 
-bool is_aisle(ll num, vll vec){
-    vll::iterator it;
-    it = find(vec.begin(),vec.end(),num);
-    if(it!=vec.end())return true;
-    else return false;
+#define MAX_SCREEN 100
+#define MAX_ROW 100
+#define MAX_COL 100
+
+typedef struct {
+    char name[50];
+    ll rows;
+    ll cols;
+    ll screen[MAX_ROW][MAX_COL];
+    ll aisles[MAX_COL];
+    ll aisle_count;
+} Screen;
+
+Screen screens[MAX_SCREEN];
+ll screen_count = 0;
+
+bool is_aisle(ll num, ll aisle[], ll aisle_count) {
+    for (ll i = 0; i < aisle_count; i++) {
+        if (aisle[i] == num) {
+            return true;
+        }
+    }
+    return false;
 }
 
-void disp(vll v){
-    for(ll i=0;i<v.size();++i){
-        if(i!=v.size()-1)cout<<v[i]<<" ";
-        else cout<<v[i];
+void disp(ll *arr, ll len) {
+    for (ll i = 0; i < len; i++) {
+        if (i != len - 1) {
+            printf("%lld ", arr[i]);
+        } else {
+            printf("%lld", arr[i]);
+        }
     }
 }
 
-void add_screen(vs str){
-    
-    
-    ll v_len = v.size();
+void add_screen(char str[][50], ll str_len) {
+    ll aisles[MAX_COL];
+    ll aisle_count = 0;
     bool flag = false;
-    FOR(i,0,v_len){
-        if(v[i].first==str[1]){
+
+    for (ll i = 0; i < screen_count; i++) {
+        if (strcmp(screens[i].name, str[1]) == 0) {
             flag = true;
             break;
         }
     }
-    if(flag){
-        cout<<"failure"<<endl;
+
+    if (flag) {
+        printf("failure\n");
         return;
     }
-    ll len = str.size();
-    //cout<<"length of str"<<len<<"    ";
-    vll aisle;
-    FOR(i,4,len){
-        aisle.pb(stoll(str[i])-1);
+
+    for (ll i = 4; i < str_len; i++) {
+        aisles[aisle_count++] = atoll(str[i]) - 1;
     }
-    string name = str[1];
-    pair<string, vector<vll > > p;
-    vector<vll > mat;
-    p.first = name;
-    ll rows = stoll(str[2]);
-    ll cols = stoll(str[3]);
-    FOR(i,0,rows){
-        vll row;
-        FOR(j,0,cols){
-            if(is_aisle(j,aisle))row.pb(2);
-            else row.pb(0);
+
+    Screen new_screen;
+    strcpy(new_screen.name, str[1]);
+    new_screen.rows = atoll(str[2]);
+    new_screen.cols = atoll(str[3]);
+    new_screen.aisle_count = aisle_count;
+
+    for (ll i = 0; i < new_screen.rows; i++) {
+        for (ll j = 0; j < new_screen.cols; j++) {
+            if (is_aisle(j, aisles, aisle_count)) {
+                new_screen.screen[i][j] = 2;  // aisle
+            } else {
+                new_screen.screen[i][j] = 0;  // empty seat
+            }
         }
-        mat.pb(row);
     }
-    p.second = mat;
-    v.pb(p);
-    //disp(aisle);
-    cout<<"success"<<endl;
-    return;
+
+    screens[screen_count++] = new_screen;
+    printf("success\n");
 }
 
-void reserve_seat(vs str){
-    ll v_len = v.size(),index = 0;
+void reserve_seat(char str[][50], ll str_len) {
     bool flag = false;
-    FOR(i,0,v_len){
-        if(v[i].first==str[1]){
+    ll index = 0;
+
+    for (ll i = 0; i < screen_count; i++) {
+        if (strcmp(screens[i].name, str[1]) == 0) {
             flag = true;
             index = i;
             break;
         }
     }
-    if(flag==false){
-        cout<<"failure"<<endl;
+
+    if (!flag) {
+        printf("failure\n");
         return;
     }
-    ll len = str.size();
-    vector<vll > screen = v[index].second;
-    ll row = stoll(str[2]) - 1;
-    vll  res_col;
-    FOR(i,3,len){
-        res_col.pb(stoll(str[i])-1);
+
+    ll row = atoll(str[2]) - 1;
+    ll res_col[MAX_COL];
+    ll res_col_count = 0;
+
+    for (ll i = 3; i < str_len; i++) {
+        res_col[res_col_count++] = atoll(str[i]) - 1;
     }
+
     flag = true;
-    FOR(i,0,res_col.size()){
-        if(screen[row][res_col[i]]==1||screen[row][res_col[i]]==3){
+    for (ll i = 0; i < res_col_count; i++) {
+        if (screens[index].screen[row][res_col[i]] == 1 || screens[index].screen[row][res_col[i]] == 3) {
             flag = false;
             break;
         }
     }
-    if(flag==false){
-        cout<<"failure"<<endl;
+
+    if (!flag) {
+        printf("failure\n");
         return;
     }
-    else{
-        //disp(res_col);
-        FOR(i,0,res_col.size()){
-            if(screen[row][res_col[i]]==0){
-                screen[row][res_col[i]] = 1;
-            }
-            else if(screen[row][res_col[i]]==2){
-                screen[row][res_col[i]] = 3;
-            }
+
+    for (ll i = 0; i < res_col_count; i++) {
+        if (screens[index].screen[row][res_col[i]] == 0) {
+            screens[index].screen[row][res_col[i]] = 1;  // reserved
+        } else if (screens[index].screen[row][res_col[i]] == 2) {
+            screens[index].screen[row][res_col[i]] = 3;  // aisle with reservation
         }
-        v[index].second = screen;
-        cout<<"success"<<endl;
-        return;
     }
+
+    printf("success\n");
 }
 
-void get_unres(vs str){
-    ll v_len = v.size(),index = 0;
+void get_unres(char str[][50], ll str_len) {
     bool flag = false;
-    FOR(i,0,v_len){
-        if(v[i].first==str[1]){
+    ll index = 0;
+
+    for (ll i = 0; i < screen_count; i++) {
+        if (strcmp(screens[i].name, str[1]) == 0) {
             flag = true;
             index = i;
             break;
         }
     }
-    if(flag==false){
-        cout<<"failure"<<endl;
+
+    if (!flag) {
+        printf("failure\n");
         return;
     }
-    
-    vector<vll > screen = v[index].second;
-    ll row = stoll(str[2]) - 1;
-    vll pri;
-    FOR(i,0,screen[0].size()){
-        if(screen[row][i]==0||screen[row][i]==2){
-            pri.pb(i+1);
+
+    ll row = atoll(str[2]) - 1;
+    ll pri[MAX_COL];
+    ll pri_count = 0;
+
+    for (ll i = 0; i < screens[index].cols; i++) {
+        if (screens[index].screen[row][i] == 0 || screens[index].screen[row][i] == 2) {
+            pri[pri_count++] = i + 1;
         }
     }
-    disp(pri);
+
+    disp(pri, pri_count);
+    printf("\n");
 }
 
-ll min(ll a,ll b){
-    if(a>b)return b;
-    else return a;
-}
-
-ll max(ll a,ll b){
-    if(a>b)return a;
-    else return b;
-}
-
-bool occupied(vll v){
+bool occupied(ll *v, ll len) {
     bool flag = true;
-    FOR(i,0,v.size()){
-        if(v[i]==1||v[i]==3){
-            flag = false;
-            break;
-        }
-    }
-    if(v.size()==1)return flag;
-    FOR(i,1,v.size()-1){
-        if(v[i]==2){
+    for (ll i = 0; i < len; i++) {
+        if (v[i] == 1 || v[i] == 3) {
             flag = false;
             break;
         }
@@ -170,95 +172,88 @@ bool occupied(vll v){
     return flag;
 }
 
-void suggest_cont(vs str){
-    ll v_len = v.size(),index = 0;
+void suggest_cont(char str[][50], ll str_len) {
     bool flag = false;
-    FOR(i,0,v_len){
-        if(v[i].first==str[1]){
+    ll index = 0;
+
+    for (ll i = 0; i < screen_count; i++) {
+        if (strcmp(screens[i].name, str[1]) == 0) {
             flag = true;
             index = i;
             break;
         }
     }
-    if(flag==false){
-        cout<<"failure"<<endl;
+
+    if (!flag) {
+        printf("failure\n");
         return;
     }
-    vector<vll > screen = v[index].second;
-    ll row = stoll(str[3]) - 1;
-    ll choice = stoll(str[4]) - 1;
-    ll num = stoll(str[2]);
-    vector<vll > pri;
-    if(screen[row][choice]==1||screen[row][choice]==3){
-        cout<<"none"<<endl;
+
+    ll row = atoll(str[3]) - 1;
+    ll choice = atoll(str[4]) - 1;
+    ll num = atoll(str[2]);
+    ll pri[MAX_COL][MAX_COL];
+    ll pri_count = 0;
+
+    if (screens[index].screen[row][choice] == 1 || screens[index].screen[row][choice] == 3) {
+        printf("none\n");
         return;
     }
-    ll len = screen[0].size();
-    FOR(i,min(0,num-choice),min(len,num+choice+1)){
-        vll seats;
-        vll indices;
-        FOR(j,i,i+num){
-            seats.pb(screen[row][j]);
-            indices.pb(j+1);
+
+    for (ll i = (choice - num + 1 < 0 ? 0 : choice - num + 1); i < (choice + num + 1 > screens[index].cols ? screens[index].cols : choice + num + 1); i++) {
+        ll seats[MAX_COL];
+        ll indices[MAX_COL];
+        for (ll j = i; j < i + num; j++) {
+            seats[j - i] = screens[index].screen[row][j];
+            indices[j - i] = j + 1;
         }
-        if(occupied(seats))pri.pb(indices);
+        if (occupied(seats, num)) {
+            for (ll k = 0; k < num; k++) {
+                pri[pri_count][k] = indices[k];
+            }
+            pri_count++;
+        }
     }
-    if(pri.size()==0)cout<<"none"<<endl;
-    else{
-        FOR(i,0,pri.size()){
-            disp(pri[i]);
-            cout<<"\n";
+
+    if (pri_count == 0) {
+        printf("none\n");
+    } else {
+        for (ll i = 0; i < pri_count; i++) {
+            disp(pri[i], num);
+            printf("\n");
         }
     }
 }
 
-int main(){
+int main() {
     ll t;
-    string test;
-    getline(cin,test);
-    t = stoll(test);
-    while(t--){
-        string s;
-        cin.clear();
-        getline(cin,s);
-        vs str;
-        
-        string temp="";
-        FOR(i,0,s.length()){
-            while(i<s.length()&&s[i]!=' ')
-            {temp+=s[i++];}
-            str.pb(temp);
-            if(i>=s.length())break;
-            temp = "";
+    scanf("%lld\n", &t);
+
+    while (t--) {
+        char str[10][50];
+        char line[500];
+        fgets(line, sizeof(line), stdin);
+
+        ll str_len = 0;
+        char *token = strtok(line, " \n");
+        while (token != NULL) {
+            strcpy(str[str_len], token);
+            str_len++;
+            token = strtok(NULL, " \n");
         }
-        if(str[0]=="add-screen"){
-            //cout<<"screenadded";
-            //FOR(i,0,str.size())cout<<str[i]<<"\n";
-            add_screen(str);
-        }
-        else if(str[0]=="reserve-seat"){
-            //cout<<"reserveseat";
-            reserve_seat(str);
-        }
-        else if(str[0]=="get-unreserved-seats"){
-            get_unres(str);
-        }
-        else if(str[0]=="suggest-contiguous-seats"){
-            suggest_cont(str);
-        }
-        else{
-            cout<<"failure"<<endl;
+
+        if (strcmp(str[0], "add-screen") == 0) {
+            add_screen(str, str_len);
+        } else if (strcmp(str[0], "reserve-seat") == 0) {
+            reserve_seat(str, str_len);
+        } else if (strcmp(str[0], "get-unreserved-seats") == 0) {
+            get_unres(str, str_len);
+        } else if (strcmp(str[0], "suggest-contiguous-seats") == 0) {
+            suggest_cont(str, str_len);
+        } else {
+            printf("failure\n");
         }
     }
-    
+
+    return 0;
 }
-/*
-7
-add-screen Screen1 12 10 4 5 8 9
-add-screen Screen2 20 25 3 4 12 13 17 18
-reserve-seat Screen1 4 5 6 7
-reserve-seat Screen2 13 6 7 8 9 10
-reserve-seat Screen2 13 4 5 6
-get-unreserved-seats Screen2 13
-suggest-contiguous-seats Screen2 4 13 4
- */
